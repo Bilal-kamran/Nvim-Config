76de18ebe331aa74566f70c59fe1f1706fd7cea5
Bilal Kamran <uroojkamran24@gmail.com>
2024-12-07 10:47:35 +0500

    Initial commit of Neovim configuration

---
+++ init.lua
@@ -0,0 +1,5 @@
+require("bilalkamran")
+require("config")
+
+-- the line below is not required as lazy.nvim automatically loads anything that is returning a table inside of the plugins directory
+-- require("plugins")
